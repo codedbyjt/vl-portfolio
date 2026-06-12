a7e41d7b63acb49b3a0127b378cd37249f4e094f
jt <jem@example.com>
2026-06-12 10:00:35 +0100

    Updates

--- assets/ProductPage-CqYNWtlF.js
+++ assets/ProductPage-CqYNWtlF.js
@@ -1,4 +1,4 @@
-import{c as m,u as p,e as u,b as r,j as e}from"./index-Cco57spv.js";import{f as h}from"./formatPrice-ChijkAgy.js";import{s as g}from"./supabase-B_Jp2-RT.js";/**
+import{c as m,u as p,e as u,b as r,j as e}from"./index-H2QTlz_K.js";import{f as h}from"./formatPrice-ChijkAgy.js";import{s as g}from"./supabase-B_Jp2-RT.js";/**
  * @license lucide-react v0.487.0 - ISC
  *
  * This source code is licensed under the ISC license.
